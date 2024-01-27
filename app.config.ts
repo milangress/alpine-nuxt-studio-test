e2056ff45b25b62a9d144548c928aaf8eaca682e
Milan G <dev@milangress.de>
2024-01-27 04:55:30 +0100

    uzb

--- app.config.ts
+++ app.config.ts
@@ -37,5 +37,11 @@ export default defineAppConfig({
     form: {
       successMessage: 'Message sent. Thank you!'
     }
+  },
+
+  prose: {
+    h1: {
+      icon: 'i-material-symbols-avg-pace'
+    }
   }
 })
